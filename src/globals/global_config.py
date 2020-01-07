e5058f5d0be037d4451076af073b3e53e4608572
zhangzhi <zhangzhi@botpy.com>
2020-01-07 13:30:07 +0800

    添加记住账号

--- src/globals/global_config.py
+++ src/globals/global_config.py
@@ -1,7 +1,44 @@
 #!/usr/bin/env python3.6
 # -*- coding: utf-8 -*-
+import json
+import os
+
 from src.globals.globals import GlobalContainer
+
+
+local_path = os.path.join(os.getcwd(), "src/globals/local.json")
+
+
+def get_local_info():
+    """
+    获取本地信息
+    """
+    if os.path.exists(local_path):
+        with open(local_path, "r+") as f:
+            json_info = json.load(f)
+    else:
+        json_info = {}
+        with open(local_path, "w") as w:
+            json.dump(json_info, w)
+    return json_info
+
+
+def set_local_info(key, value):
+    """
+    保存本地信息
+    :return:
+    """
+    with open(local_path, "r+") as f:
+        json_info = json.load(f)
+        json_info[key] = value
+        f.seek(0)
+        json.dump(json_info, f)
+
 
 global_config = GlobalContainer()
 global_config.hostname = 'http://joucks.cn:3344'
 global_config.ws_server = ['http://joucks.cn:3356', 'http://joucks.cn:3358']
+
+
+local_info = get_local_info()
+global_config.username = local_info.get("username", "")
--- src/ydxx_panel/login_panel.py
+++ src/ydxx_panel/login_panel.py
@@ -1,30 +1,55 @@
-import urwid
-from src.globals.global_values import global_values
-from src.ydxx_panel.main_panel import MainPanel
-import json
-from src.globals.api import api
-from src.ydxx_widget import button
-import aiohttp
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+登录面板
+"""
 import asyncio
 
+import aiohttp
+import urwid
+
+from src.globals.api import api
+from src.globals.global_config import global_config
+from src.globals.global_config import set_local_info
+from src.globals.global_values import global_values
+from src.ydxx_panel.main_panel import MainPanel
+from src.ydxx_widget import button
+
+
 class LoginPanel(urwid.WidgetPlaceholder):
+    """
+    登录面板
+    """
+
     def __init__(self):
+
         def on_login(button):
-            uname = self.uname_edit.get_edit_text()
+            username = self.username_edit.get_edit_text()
+            set_local_info("username", username)
             pwd = self.pwd_edit.get_edit_text()
-            asyncio.get_event_loop().create_task(self.login(uname, pwd))
+            asyncio.get_event_loop().create_task(self.login(username, pwd))
+
         super(LoginPanel, self).__init__(urwid.SolidFill(' '))
+        # 设置登录页面的logo
         logo = urwid.Text('')
 #        with open('./logo.txt') as logo_file:
 #            logo.set_text(logo_file.read())
-        self.uname_edit = urwid.Edit('账号: ')
+        self.username_edit = urwid.Edit('账号: ')
+        if global_config.username:
+            self.username_edit.set_edit_text(global_config.username)
         self.pwd_edit = urwid.Edit('密码: ', mask='*')
         login_btn = button.Button('[登陆]', on_press=on_login)
-        login_btn_wp = urwid.Padding(
-            login_btn, align=urwid.CENTER, width='pack')
+        login_btn_wp = urwid.Padding(login_btn, align=urwid.CENTER, width='pack')
         self.login_result_info = urwid.Text('', align=urwid.CENTER)
-        form = urwid.LineBox(urwid.Pile(
-            [self.uname_edit, self.pwd_edit, self.login_result_info, login_btn_wp], focus_item=0))
+        form = urwid.LineBox(
+            urwid.Pile(
+                [
+                    self.username_edit, self.pwd_edit,
+                    self.login_result_info, login_btn_wp
+                ],
+                focus_item=0
+            )
+        )
         self.original_widget = urwid.Overlay(
             urwid.ListBox(urwid.SimpleFocusListWalker([logo, form])),
             self.original_widget,
@@ -33,9 +58,14 @@ class LoginPanel(urwid.WidgetPlaceholder):
             min_width=24, min_height=8,
             left=10, right=10)
 
-    async def login(self, uname, pwd):
+    async def login(self, username, pwd):
+        """
+        登录
+        """
         session = aiohttp.ClientSession()
-        async with session.post(api.login, data={'user_name': uname, 'user_pwd': pwd}) as response:
+        async with session.post(
+                api.login, data={'user_name': username, 'user_pwd': pwd}
+        ) as response:
             json_rst = await response.json()
             if 'code' in json_rst and json_rst['code'] == 200:
                 user_data = json_rst['data']
